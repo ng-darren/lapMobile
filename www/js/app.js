0c7d667efa4bd0f3e50386c6e389d34368cd3dcc
Darren <ngmunyong@Darrens-MacBook-Pro.local>
2015-06-19 07:09:07 +1000

    Login/register page

--- www/js/app.js
+++ www/js/app.js
@@ -1,67 +1,70 @@
-angular.module('lapMobile', ['ionic', 'firebase', 'lapMobile.frame', 'lapMobile.items', 'lapMobile.camera', 'lapMobile.main', 'lapMobile.elasticTextarea'])
-.constant('FIREBASEURL', 'https://lapMobile.firebaseio.com/')
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/frame.html",
-    controller: 'AppCtrl'
-  })
+angular.module('lapMobile', [
+    'ionic',
+    'firebase',
+    'angularMoment',
+    'lapMobile.main',
+    'lapMobile.frame',
 
-  // .state('app.search', {
-  //   url: "/search",
-  //   views: {
-  //     'menuContent': {
-  //       templateUrl: "templates/search.html"
-  //     }
-  //   }
-  // })
+    'lapMobile.items',  // Services
+    'lapMobile.camera',
 
-  .state('app.main', {
-    url: "/main",
-    views: {
-      'menuContent': {
-        templateUrl: "views/main/main.html"
-      }
-    }
-  })
-  //   .state('app.playlists', {
-  //     url: "/playlists",
-  //     views: {
-  //       'menuContent': {
-  //         templateUrl: "templates/playlists.html",
-  //         controller: 'PlaylistsCtrl'
-  //       }
-  //     }
-  //   })
+    'lapMobile.register',   // Views
 
-  // .state('app.single', {
-  //   url: "/playlists/:playlistId",
-  //   views: {
-  //     'menuContent': {
-  //       templateUrl: "templates/playlist.html",
-  //       controller: 'PlaylistCtrl'
-  //     }
-  //   }
-  // });
-  // 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/main');
-})
-.config(function($compileProvider){
-  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-});
+    'lapMobile.elasticTextarea' // Directives
+    ])
+    .constant('FIREBASEURL', 'https://lapMobile.firebaseio.com/')
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('app', {
+            url: "/app",
+            abstract: true,
+            templateUrl: "templates/frame.html",
+            controller: 'AppCtrl'
+        })
+        .state('app.main', {
+            url: "/main",
+            views: {
+                'menuContent': {
+                    templateUrl: "views/main/main.html",
+                    controller: 'MainCtrl'
+                }
+            }
+        })
+        .state('app.register', {
+            url: "/register",
+            views: {
+                'menuContent': {
+                    templateUrl: "views/register/register.html",
+                    controller: 'RegisterCtrl'
+                }
+            }
+        })
+
+      // .state('app.single', {
+      //   url: "/playlists/:playlistId",
+      //   views: {
+      //     'menuContent': {
+      //       templateUrl: "templates/playlist.html",
+      //       controller: 'PlaylistCtrl'
+      //     }
+      //   }
+      // });
+      // 
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/app/register');
+    })
+    .config(function($compileProvider){
+      $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+    })
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    });
