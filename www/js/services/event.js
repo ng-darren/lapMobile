116b7c9179b18d25f01374adc065f4520387498d
Darren <ngmunyong@Darrens-iMac.local>
2015-06-22 23:32:57 +1000

    structure brainstroming
    Probably doesn’t need a separate seed list.

--- www/js/services/event.js
+++ www/js/services/event.js
@@ -10,51 +10,25 @@ angular.module('lapMobile.event', []).factory('Event', function ($firebaseArray,
         return $firebaseArray(eventRef);
       },
       getLastTen: function () {
-      	var query = eventRef.orderByKey().limitToLast(2);
+      	var query = eventRef.orderByKey().limitToLast(4);
 
         return $firebaseArray(query);
       },
-      findByItemID: function (topicID) {
-        // if (topicID) {
-        //   ref = new Firebase(FIREBASE_URL + '/forum/' + topicID);
-        //   sync = $firebase(ref);
-        //   obj = sync.$asObject();
-
-        //   obj.$loaded().then(function() {
-
-        //   });
-        //   return obj;
-        // } else {
-        //   return null;
-        // }
+      findByEventID: function (topicID) {
+        
       },
       createEvent: function (name, description) {
       	eventArray = $firebaseArray(eventRef);
       	return eventArray.$add({
             'name': name,
             'description': description,
-            'timestamp': Date.now()
+            'timestamp': Date.now(),
+            'parent': null,
+            'child': {},
+            'mediaUrl': {},
+            'likes': {},
+            'comments': {}
         });
-
-        // ref = new Firebase(FIREBASE_URL + '/forum/' + topicID + '/messages');
-        // sync = $firebase(ref);
-
-        // var createdBy = $rootScope.user.firstname;
-        // var createdOn = Date.now();
-        // var image = $rootScope.user.image;
-
-        // if (msg) {
-        //   sync.$push({ 
-        //     createdBy: createdBy,
-        //     createdOn: createdOn, 
-        //     image: image,
-        //     message: msg 
-        //    }).then(function() {
-        //     ForumIndex.newUpdate(topicID, createdBy, createdOn, image);
-        //   }, function(error) {
-        //     console.log('Error:', error);
-        //   });
-        // }
       },
       createFile: function (topicID, filename) {
         // ref = new Firebase(FIREBASE_URL + '/forum/' + topicID + '/messages');
